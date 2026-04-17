33192e4d6cd003a8ba50b4a5e97421e44e829faa
Your Name <alisapodgaynaya023@gmail.com>
2026-04-17 13:14:37 -0600

    MOCK: add placeholder data to pages

---
+++ app/catalog/[camperId]/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function CamperDetails() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <p className={styles.p}>hello one camper</p>
+      </main>
+    </div>
+  );
+}
---
+++ app/catalog/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Campers() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <p className={styles.p}>hello campers</p>
+      </main>
+    </div>
+  );
+}
