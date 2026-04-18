fc0dae0643013e9fd02dd2fe95802000f084f209
Your Name <alisapodgaynaya023@gmail.com>
2026-04-17 21:34:44 -0600

    FEAT: add load more logic with placeholder render

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,11 +1,51 @@
-// import Image from "next/image";
+"use client";
 import styles from "./CatalogPage.module.css";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchCampers } from "@/lib/campersApi";
+import { queryKeys } from "@/lib/queryKeys";
 
 export default function Campers() {
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: queryKeys.campers,
+    queryFn: ({ pageParam }) => fetchCampers({ page: pageParam, perPage: 4 }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      if (lastPage.page < lastPage.totalPages) return lastPage.page + 1;
+      return undefined;
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong.</p>;
+
+  const campersList = data?.pages.flatMap((page) => page.campers) ?? [];
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p className={styles.p}>hello campers</p>
+        <div className={styles.filtersWrapper}></div>
+        <div className={styles.contentWrapper}>
+          <p className={styles.p}>hello campers</p>
+          {campersList.map((camper) => (
+            <div key={camper.id}>
+              <p>{camper.name}</p>
+              <p>{camper.price}</p>
+              <p>{camper.location}</p>
+            </div>
+          ))}
+
+          {hasNextPage && (
+            <button onClick={() => fetchNextPage()}>
+              {isFetchingNextPage ? "Loading..." : "Load more"}
+            </button>
+          )}
+        </div>
       </main>
     </div>
   );
